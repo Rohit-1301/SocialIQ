62bf51c62269a49f45c43ea01a8b211fa1a918a5
Rohit-1301 <148363170+Rohit-1301@users.noreply.github.com>
2025-01-08 23:25:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://api.langflow.astra.datastax.com',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
